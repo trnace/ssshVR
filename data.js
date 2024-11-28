728dc5b96ae3f04548aa7627fec895e5063ff750
trnace <ray0636enen@gmail.com>
2024-11-28 11:48:12 +0800

    1128

---
+++ data.js
@@ -0,0 +1,591 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1",
+      "name": "北大特區出入口-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3506723772750124,
+          "pitch": 0.06929604931338496,
+          "rotation": 7.0685834705770345,
+          "target": "2-"
+        },
+        {
+          "yaw": -2.662290849452896,
+          "pitch": 0.07811167291381693,
+          "rotation": 11.780972450961727,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2751844594804744,
+          "pitch": -0.14364739592874542,
+          "title": "福爾摩沙高速公路",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-andand",
+      "name": "公園and購物大樓and圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.640694225173286,
+          "pitch": 0.021604766505925355,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.9352563728868093,
+          "pitch": 0.02553987964018134,
+          "rotation": 4.71238898038469,
+          "target": "9-"
+        },
+        {
+          "yaw": -0.07199109448044894,
+          "pitch": 0.06148335678516759,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "北大高中",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9633170336092434,
+          "pitch": 0.12246770136762741,
+          "rotation": 0,
+          "target": "0--1"
+        },
+        {
+          "yaw": 0.03332641877525511,
+          "pitch": 0.09218761190517633,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3756322738680913,
+          "pitch": 0.08227037038296459,
+          "title": "北大高中",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "台北大學",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.011920441541660054,
+          "pitch": 0.11659285921965612,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -1.6647571441525173,
+          "pitch": 0.04566815241476796,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "家樂福",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25885123441685387,
+          "pitch": 0.01416274120360228,
+          "rotation": 1.5707963267948966,
+          "target": "5-"
+        },
+        {
+          "yaw": -3.0827278198328294,
+          "pitch": 0.10509361369519432,
+          "rotation": 7.0685834705770345,
+          "target": "1-andand"
+        },
+        {
+          "yaw": -0.1685638693550704,
+          "pitch": 0.028375508622158208,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.918446763978423,
+          "pitch": -0.18893782703379003,
+          "title": "家樂福北大店",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "桃子腳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.139221269226626,
+          "pitch": 0.09690223933824882,
+          "rotation": 0,
+          "target": "0--1"
+        },
+        {
+          "yaw": -0.03476635692789465,
+          "pitch": 0.036495989010498775,
+          "rotation": 11.780972450961727,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6232922841196844,
+          "pitch": 0.05557300002507759,
+          "title": "新北市立桃子腳國民中小學",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "教育研究院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1245590350775645,
+          "pitch": 0.11813691418420191,
+          "rotation": 0,
+          "target": "10-"
+        },
+        {
+          "yaw": 1.295686917401257,
+          "pitch": 0.37162657595878024,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "麥當勞",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0767004051719482,
+          "pitch": 0.11708680408197658,
+          "rotation": 0,
+          "target": "10-"
+        },
+        {
+          "yaw": -1.5507444917525959,
+          "pitch": 0.1441243872301854,
+          "rotation": 0,
+          "target": "1-andand"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "街區內側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.843386570601865,
+          "pitch": -0.040504503258361524,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.2530852243045825,
+          "pitch": 0.1333543415452887,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "樹林三峽分界",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.090107293580413,
+          "pitch": -0.0035604760149663406,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.34373984080873576,
+          "pitch": 0.005403174254585252,
+          "rotation": 1.5707963267948966,
+          "target": "1-andand"
+        },
+        {
+          "yaw": -0.002192859456885188,
+          "pitch": 0.06588834818170142,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.7144863641350172,
+          "pitch": 0.07350855814418367,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "籠埔國小",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.976035527804738,
+          "pitch": 0.11281815269692785,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.15039252708034567,
+          "pitch": -0.028286420994396266,
+          "rotation": 1.5707963267948966,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "北大特區",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
